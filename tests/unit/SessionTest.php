1c1408150a27163e4e17f2432f79550bcc94bee5
vagrant <vagrant@precise64.(none)>
2014-12-12 07:49:11 +0000

    Initialize test SessionController

---
+++ tests/unit/SessionTest.php
@@ -0,0 +1,19 @@
+<?php
+
+class SessionTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+    }
+
+    protected function tearDown()
+    {
+    }
+
+    // tests
+    public function testMe()
+    {
+        $this->assertTrue(false);
+    }
+
+}
